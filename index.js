8622657e7964d80fb59c2820a9624091f0e17ef1
Nandakumar <49996841+AvulaNandakumar@users.noreply.github.com>
2024-08-04 14:21:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.listen(3000, () => console.log("server is running"));
 
 //calling api
 
-app.use("/",express.static(path.join(__dirname,"index.html")));
+app.get("/",(req,res)=>res.send("enter city name as a path parameter to get weather information of the city."));
 app.get("/:city/",(req, res) => {
 
     let { city } = req.params;
